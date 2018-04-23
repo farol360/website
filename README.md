e7518b0f1c544e462c53fd4307c1e7431f5bc06f
thnardi <thnardi@gmail.com>
2018-04-23 12:40:31 -0300

    news on readme

--- README.md
+++ README.md
@@ -15,30 +15,31 @@ This is a tool built by [Maurício Fauth](https://github.com/mauriciofauth) and 
 
 This section's objective is to guide anyone ho wants to work with ancora, explain his basic usage.
 
-The core of application are built with [Slim 3 Framework](https://www.slimframework.com). To understand the code you must focus on slim's *routes* and *dependecies* management and learn about his *middleware* concept.
+The core of application are built with [Slim 3 Framework](https://www.slimframework.com). To understand the code you must focus on slim's *routes* and *dependecies* management and learn about his [middleware](https://www.slimframework.com/docs/v3/concepts/middleware.html) concept.
 
-All the solution is implemented on *app/* folder. The core files are **dependencies.php**, **middleware.php**, **routes.php** and **settings.php**. All this files contains declarations of elements we work on implemented solution. The code in it's default have the basic permissions for admin users, flash messages and Twig engine for render the view layer. You can use this implementations as template to adding new declarations as it needs.
+All the solution is implemented on *app/* folder. The core files are **dependencies.php**, **middleware.php**, **routes.php** and **settings.php**. All this files contains declarations of elements we work on. The code in it's default have declarations like the basic permissions controllers, flash messages, loggers with monolog and Twig engine for render the view layer. You can use this implementations as template to adding new declarations as it needs.
 
 The MVC concept are strongly implemented on this solution. Let's take an overview by MVC layers.
 
 ### Model layer
 
-The model is implemented on *src/model/* folder. It work with 2 classes: a plain class refering exactly the table in db and a same name ended by **Model** pair class with extends *src/Model.php* class. This class have all sql functions.
+The model is implemented on *src/model/* folder. It work with 2 classes: a plain one refering exactly the table in db and a same name ended by **Model** pair class that extends *src/Model.php* with have all sql functions.
 
 The way to add a new class is:
 1) Supose you have an example table in you db. Create the **Example.php** in *src/model* (you can take any other class already implemented as template);
 2) Create a **ExampleModel.php** in same directory;
 3) Insert a function on *src/model/EntityFactory.php* with an array $data as parameter and return a new Example instance. (this is a Factory design pattern implementation);
-4) Add the **exampleModel** class as atribute in controller you want to work. You will need to pass it as parameter in contructor and update constructor call on *src/dependencies* file.
+4) Add the **exampleModel** class as atribute in controller you want to work. You will need to pass it as parameter in contructor and update *src/dependencies* file.
 
 ### View
 
 In View we use [Twig](https://twig.symfony.com/) engine.
 
-Functions used in layout.twig like "get_name()" or "get_email()" are defined in src/twig/AuthExtension.php file;
+- Functions used in layout.twig like "get_name()" or "get_email()" are defined in src/twig/AuthExtension.php file;
+
 
 ### Controller
 
-All controller classes extens *src/Controller.php*
+All controller classes extends *src/Controller.php* and is stored on *src/controller* folder. You can pass dependencies (e.g. models) in class constructor declaration, setting his attributes.
 
-
+Functions that render views need a strict parameter signature. You can check out default controllers as a guide and learn on [Slim 3 Docs](https://www.slimframework.com/docs/). All you need to know by now is the Request, and Response parameters are obrigatory. You can use an third optional array parameter called $args to get variable URL parts.
--- app/routes.php
+++ app/routes.php
@@ -2,17 +2,13 @@
 declare(strict_types=1);
 
 // includes
-use Farol360\IsepeEventos\Controller\Admin\EventsController as EventsAdmin;
-use Farol360\IsepeEventos\Controller\Admin\EventsTypeController as EventsTypeAdmin;
-use Farol360\IsepeEventos\Controller\Admin\IndexController as IndexAdmin;
-use Farol360\IsepeEventos\Controller\Admin\PermissionController as PermissionAdmin;
-use Farol360\IsepeEventos\Controller\Admin\RoleController as RoleAdmin;
-use Farol360\IsepeEventos\Controller\Admin\TrashController as TrashAdmin;
-use Farol360\IsepeEventos\Controller\Admin\UserController as UserAdmin;
-use Farol360\IsepeEventos\Controller\Admin\SubscriptionController as SubscriptionAdmin;
+use Farol360\Ancora\Controller\Admin\IndexController as IndexAdmin;
+use Farol360\Ancora\Controller\Admin\PermissionController as PermissionAdmin;
+use Farol360\Ancora\Controller\Admin\RoleController as RoleAdmin;
+use Farol360\Ancora\Controller\Admin\UserController as UserAdmin;
 
-use Farol360\IsepeEventos\Controller\PageController as Page;
-use Farol360\IsepeEventos\Controller\UserController as User;
+use Farol360\Ancora\Controller\PageController as Page;
+use Farol360\Ancora\Controller\UserController as User;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -21,41 +17,6 @@ $app->get('[/]', Page::class . ':index');
 
 $app->group('/admin', function () {
     $this->get('[/]', IndexAdmin::class . ':index');
-
-    $this->group('/certificates', function() {
-        $this->get('[/]', EventsAdmin::class . ':certificates' );
-    });
-
-    $this->group('/events', function() {
-        $this->get('[/]', EventsAdmin::class . ':index' );
-
-        $this->map(['GET', 'POST'], '/add', EventsAdmin::class . ':add' );
-        $this->get('/disable/{id:[0-9]+}', EventsAdmin::class . ':disable');
-        $this->get('/enable/{id:[0-9]+}', EventsAdmin::class . ':enable');
-        $this->get('/edit/{id:[0-9]+}', EventsAdmin::class . ':edit' );
-        $this->map(['GET', 'POST'], '/update', EventsAdmin::class . ':update' );
-        $this->get('/trash/remove/{id:[0-9]+}', EventsAdmin::class . ':trashRemove');
-        $this->get('/trash/send/{id:[0-9]+}', EventsAdmin::class . ':trashSend');
-
-    });
-
-    $this->group('/eventstype', function () {
-        $this->get('[/]', EventsTypeAdmin::class . ':index' );
-        $this->map(['GET', 'POST'], '/add', EventsTypeAdmin::class . ':add' );
-        $this->get('/disable/{id:[0-9]+}', EventsTypeAdmin::class . ':disable');
-        $this->get('/enable/{id:[0-9]+}', EventsTypeAdmin::class . ':enable');
-        $this->get('/edit/{id:[0-9]+}', EventsTypeAdmin::class . ':edit' );
-        $this->get('/getTerms/{id:[0-9]+}', EventsTypeAdmin::class . ':getTerms' );
-        $this->map(['GET', 'POST'], '/update', EventsTypeAdmin::class . ':update' );
-
-        $this->get('/remove/{id:[0-9]+}', EventsTypeAdmin::class . ':remove' );
-        // events type dnt implements trash feature
-        //$this->get('/trash/remove/{id:[0-9]+}', EventsTypeAdmin::class . ':trashRemove');
-        //$this->get('/trash/send/{id:[0-9]+}', EventsTypeAdmin::class . ':trashSend');
-
-        $this->map(['GET', 'POST'], '/verifytoremove/{id:[0-9]+}', EventsTypeAdmin::class . ':verifytoremove' );
-        $this->map(['GET', 'POST'], '/verifytounpublish/{id:[0-9]+}', EventsTypeAdmin::class . ':verifytounpublish' );
-    });
 
     $this->group('/permission', function () {
         $this->get('[/]', PermissionAdmin::class . ':index');
@@ -72,23 +33,6 @@ $app->group('/admin', function () {
         $this->get('/edit/{id:[0-9]+}', RoleAdmin::class . ':edit');
         $this->post('/update', RoleAdmin::class . ':update');
     });
-
-    $this->group('/subscriptions', function() {
-        $this->get('[/]', SubscriptionAdmin::class . ':index');
-        $this->get('/{id:[0-9]+}', SubscriptionAdmin::class . ':index');
-        $this->get('/activate/{id:[0-9]+}', SubscriptionAdmin::class . ':activate');
-        $this->get('/deactivate/{id:[0-9]+}', SubscriptionAdmin::class . ':deactivate');
-        $this->get('/open/{id:[0-9]+}', SubscriptionAdmin::class . ':open');
-    });
-
-    $this->group('/attendances', function() {
-        $this->get('[/]', SubscriptionAdmin::class . ':attendances');
-        $this->get('/{id:[0-9]+}', SubscriptionAdmin::class . ':attendances');
-        $this->get('/activate/{id:[0-9]+}', SubscriptionAdmin::class . ':activate');
-        $this->get('/deactivate/{id:[0-9]+}', SubscriptionAdmin::class . ':deactivate');
-        $this->get('/open/{id:[0-9]+}', SubscriptionAdmin::class . ':open');
-    });
-
 
     /**
         Trash index route
@@ -109,23 +53,6 @@ $app->group('/admin', function () {
 
 $app->map(['GET', 'POST'], '/contato', Page::class . ':contato');
 
-$app->group('/eventos', function () {
-    $this->get('[/]', Page::class . ':events');
-    $this->get('/{id:[0-9]+}', Page::class . ':eventId');
-
-    $this->group('/categorias', function() {
-        $this->get('[/]', Page::class . ':eventsType');
-        $this->get('/{id:[0-9]+}', Page::class . ':eventsTypeId');
-    });
-});
-
-$app->group('/inscricao', function () {
-    $this->map(['GET', 'POST'], '[/]', Page::class . ':inscricao'); //not found any use for this yet
-    $this->get('/{id:[0-9]+}', Page::class . ':inscricao');
-    $this->post('/add', Page::class . ':inscricaoAdd');
-
-});
-
 $app->get('/obrigado', Page::class . ':contatoObrigado');
 
 $app->group('/users', function () {
@@ -137,5 +64,5 @@ $app->group('/users', function () {
     $this->get('/signout', User::class . ':signOut');
     $this->map(['GET', 'POST'], '/signup', User::class . ':signUp');
     $this->get('/verify/{token}', User::class . ':verify');
-    $this->get('/inscricoes', Page::class . ':userInscricoes');
+
 });
